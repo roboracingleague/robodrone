a4266107a55a57ec8a56094231ea7e5d752e4e6a
guillaume.pinot <guillaume.pinot@renault.com>
2020-01-20 20:50:58 +0100

    improve shell

--- update-remote.sh
+++ update-remote.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+### to push your code on your remote drone
+
+REMOTE_USER=dlinano
+
+[[ "$1" == "" ]] && echo "Error, you must specify the IP of your remote drone" && exit 1
 IPDEST=$1
-ssh dlinano@$IPDEST "rm -fr ~/robodrone/*" && scp -r -p ./* dlinano@$IPDEST:~/robodrone/.
+
+ssh $REMOTE_USER@$IPDEST "rm -fr ~/robodrone/*" && scp -r -p ./* dlinano@$IPDEST:~/robodrone/.
